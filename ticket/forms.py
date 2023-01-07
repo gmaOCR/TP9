5d495948b62c6ac8703772f3671040c41a4e1674
Greg M <marainigregory@pm.me>
2023-01-07 08:06:32 +0100

    create review form and page

--- ticket/forms.py
+++ ticket/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from . import models
 
+CHOICES = (
+    ("1",1),
+    ("2",2),
+    ("3",3),
+    ("4",4),
+    ("5",5)
+)
+
 class TicketForm(forms.ModelForm):
     edit_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
 
@@ -13,7 +21,11 @@ class DeleteTicketForm(forms.Form):
 
 class ReviewForm(forms.ModelForm):
     edit_review = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+    rating = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, label="Note")
 
     class Meta:
         model = models.Review
         fields = ['headline','rating','body']
+        # widgets = {
+        #     'rating': forms.RadioSelect(attrs={'choices': CHOICES})
+        # }
