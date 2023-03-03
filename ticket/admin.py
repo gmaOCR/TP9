4393978233baecce0223ee31edf9a6fcdafc7272
Greg M <marainigregory@pm.me>
2023-03-03 12:45:54 +0100

    Amélioration esthtique interface admin

--- ticket/admin.py
+++ ticket/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Ticket)
-admin.site.register(models.Review)
-admin.site.register(models.UserFollows)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title','user','description')
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('headline','get_title','user')
+
+    @admin.display(ordering='ticket__title', description='Titre du ticket')
+    def get_title(self, Review):
+        return Review.ticket.title
+
+class UFAdmin(admin.ModelAdmin):
+    list_display = ('user', 'followed_user')
+
+
+admin.site.register(models.Ticket, TicketAdmin)
+admin.site.register(models.Review, ReviewAdmin)
+admin.site.register(models.UserFollows, UFAdmin)
